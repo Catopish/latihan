67081e12b6cfbd2b167450a28ffbe0ffd462a9b8
Catopish <athila2001@gmail.com>
2024-05-28 22:27:31 +0700

    if else

--- Section 16/app.js
+++ Section 16/app.js
@@ -1,1 +1,9 @@
-console.log("hello world!!")
+console.log("hello world!!");
+
+let random = Math.random();
+if (random > 0.5) {
+  console.log("your number bigger than 0.5");
+} else {
+  console.log("yor number less than 0.5");
+}
+console.log(random);
