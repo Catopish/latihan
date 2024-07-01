2219cc67329e847238fb8cdf89ba370e4b245f22
Catopish <athila2001@gmail.com>
2024-07-01 23:06:25 +0700

    haduh

--- Color Gen/app22.js
+++ Color Gen/app22.js
@@ -1,15 +1,19 @@
 const colorize = ()=>{
-    let i = Math.floor(Math.random()*255);
-    let j = Math.floor(Math.random()*255);
-    let k = Math.floor(Math.random()*255);
+    const i = Math.floor(Math.random()*255);
+    const j = Math.floor(Math.random()*255);
+    const k = Math.floor(Math.random()*255);
   return `rgb(${i},${j},${k})`;
 }
 
 const buttons = document.querySelectorAll('button');
 
-for (const button of buttons) {
-  button.addEventListener('click',function(){
 
-  }) 
+const gantibagian=()=>{
+  this.style.backgroundColor=colorize();
 }
-buttons.addEventListener()
+
+
+for (let button of buttons) {
+  button.addEventListener('click',gantibagian)
+}
+
